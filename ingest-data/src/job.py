2efb09fa37d8861ab5f0d339de6d04b3ef5c7a06
Marc Jamot <marc@getstoix.com>
2021-08-25 22:56:33 +0200

    Update load in batches

--- ingest-data/src/job.py
+++ ingest-data/src/job.py
@@ -3,14 +3,23 @@
 from typing import Any, Callable, Dict, Iterable
 from transform.csv_to_bq import Output, parse_input, transform
 
+BATCH_SIZE = 1024
+
 
 def job(
-    extract: Callable[[], Iterable[Dict[str, Any]]], load: Callable[[Output], None]
+    extract: Callable[[], Iterable[Dict[str, Any]]],
+    load: Callable[[Iterable[Output]], None],
 ):
     """Core logic for our ETL job."""
+    outputs = []
     for row in extract():
         data = parse_input(row)
         if data is None:
             continue
         output = transform(data)
-        load([output])
+        outputs.append(output)
+        if len(outputs) > BATCH_SIZE:
+            load(outputs)
+            outputs = []
+    if len(outputs) > 0:
+        load(outputs)
